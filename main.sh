4bca1a4c00765f655fe4a5415ce1fd594643c61a
Lihuel <62969028+LihuelMujica@users.noreply.github.com>
2022-08-03 17:58:10 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,19 +13,19 @@ select opt in "${opciones[@]}"
 do
     case $opt in 
 
-        "Indicador estadístico de longitud de palabras") bash statsWords.sh; break
+        "Indicador estadístico de longitud de palabras") bash EDP-TPFinal-Celi/statsWords.sh; break
         ;;
 
-        "Indicador estadístico de uso de palabras") bash statsUsageWords.sh; break
+        "Indicador estadístico de uso de palabras") bash EDP-TPFinal-Celi/statsUsageWords.sh; break
         ;;
 
-        "Identificación de nombres propios") bash findNames.sh; break
+        "Identificación de nombres propios") bash EDP-TPFinal-Celi/findNames.sh; break
         ;;
 
-        "Indicador estadístico de longitud de oraciones") bash statsSentences.sh; break
+        "Indicador estadístico de longitud de oraciones") bash EDP-TPFinal-Celi/statsSentences.sh; break
         ;;
 
-        "Contador de líneas en blanco") bash blankLinesCounter.sh; break
+        "Contador de líneas en blanco") bash EDP-TPFinal-Celi/blankLinesCounter.sh; break
         ;;
 
         "salir") break 2
